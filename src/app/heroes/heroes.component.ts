dac390c9f04848fb7bbee779b84929542e4c1c82
richard <richard.liao@folkspaper.com>
2021-07-13 16:28:06 +0800

    chore: create a service using Injectable、 Observable

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,7 +9,7 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent implements OnInit {
-  constructor() {}
+  constructor(private heroService: HeroService) {}
 
   hero: Hero = {
     id: 1,
@@ -19,10 +20,17 @@ export class HeroesComponent implements OnInit {
 
   selectedHero?: Hero;
 
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
   onSelect(hero: Hero): void {
-    console.log(hero)
+    console.log(hero);
     this.selectedHero = hero;
   }
 
-  ngOnInit(): void {}
+  getHeroes(): void {
+    // this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
 }
