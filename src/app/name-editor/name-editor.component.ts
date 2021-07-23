065777fa4515aadfc8d9cef05656e1e3a48548a0
bnmghjtyu0 <bnmghjtyu0@gmail.com>
2021-07-23 17:09:50 +0800

    表單驗證 day1

---
+++ src/app/name-editor/name-editor.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
+
+@Component({
+  selector: 'name-editor',
+  templateUrl: './name-editor.component.html',
+  styleUrls: ['./name-editor.component.scss'],
+})
+export class NameEditorComponent implements OnInit {
+  profileForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: [''],
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: [''],
+    }),
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
+  updateProfile() {
+    this.profileForm.patchValue({
+      firstName: 'Nancy',
+      address: {
+        street: '123 Drew Street',
+      },
+    });
+  }
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.log(this.profileForm.status);
+    console.warn(this.profileForm.value);
+  }
+}
