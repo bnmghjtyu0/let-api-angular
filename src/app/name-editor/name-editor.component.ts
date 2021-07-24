75be63bc969ff53a13b388513223fe98345f80e1
richard <richard.liao@folkspaper.com>
2021-07-24 22:40:39 +0800

    表單驗證 day2

--- src/app/name-editor/name-editor.component.ts
+++ src/app/name-editor/name-editor.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import { FormBuilder } from '@angular/forms';
-import { Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'name-editor',
@@ -9,27 +12,76 @@ import { Validators } from '@angular/forms';
   styleUrls: ['./name-editor.component.scss'],
 })
 export class NameEditorComponent implements OnInit {
-  profileForm = this.fb.group({
-    firstName: ['', Validators.required],
-    lastName: [''],
-    address: this.fb.group({
-      street: [''],
-      city: [''],
-      state: [''],
-      zip: [''],
-    }),
-  });
-
+  countryList: { id: number; name: string }[] = [
+    { id: 1, name: 'Los Angeles' },
+    { id: 2, name: 'San Francisco' },
+  ];
+  profileForm!: FormGroup;
+  registerForm() {
+    this.profileForm = this.fb.group({
+      userName: [
+        '',
+        [
+          Validators.required,
+          // Validators.pattern(/.+@.+\..+/),
+          Validators.minLength(5),
+        ],
+      ],
+      nickName: [''],
+      country: [1, Validators.required],
+      county: [''],
+      passwordGroup: this.fb.group(
+        {
+          password: [''],
+          passwordConfirm: [''],
+        },
+        { validator: this.passwordMatchValidator }
+      ),
+    });
+  }
   constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.registerForm();
+  }
+  get userName() {
+    return this.profileForm.controls['userName'];
+  }
+  get country() {
+    return this.profileForm.controls['country'];
+  }
+  get county() {
+    return this.profileForm.controls['county'];
+  }
+
+  passwordMatchValidator(frm: FormGroup) {
+    return frm.controls['password'].value ===
+      frm.controls['passwordConfirm'].value
+      ? null
+      : { mismatch: true };
+  }
+  onCountryChange() {
+    let countrySelected = this.country.value;
+    if (countrySelected === 2) {
+      this.county.setValidators([Validators.required]);
+      this.county.updateValueAndValidity();
+    } else {
+      this.county.clearValidators();
+      this.county.updateValueAndValidity();
+    }
+  }
   updateProfile() {
     this.profileForm.patchValue({
-      firstName: 'Nancy',
-      address: {
-        street: '123 Drew Street',
-      },
+      userName: 'richard',
+      nickName: 'richard',
+      // password: {
+      //   password: '123 Drew Street',
+      // },
     });
+  }
+
+  resetFrom() {
+    this.profileForm.reset();
   }
   onSubmit() {
     // TODO: Use EventEmitter with form value
