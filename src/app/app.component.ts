b8f83f77501760d1ec523d631e940c686e6f30ff
richard <richard.liao@folkspaper.com>
2021-08-10 16:22:52 +0800

    service: media-query

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { MediaQueryService } from './services/media-query.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'let-api-angular';
+  mediaQuery: string = '';
+  constructor(private mediaQueryService: MediaQueryService) {
+    this.mediaQueryService.obob.subscribe((result: any) => {
+      for (const query of Object.keys(result.breakpoints)) {
+        if (result.breakpoints[query]) {
+          // console.log(this.displayNameMap.get(query));
+          this.mediaQuery =
+            this.mediaQueryService.displayNameMap.get(query) ?? 'Unknown';
+        }
+      }
+    });
+  }
 }
