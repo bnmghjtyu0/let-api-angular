7be353390d7140a0d058a6e7213095ed3be75609
Mr.Richard <bnmghjtyu0@gmail.com>
2021-08-30 21:30:40 +0800

    install @ngrx/store-devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 import { counterReducer } from './counter.reducer';
 @NgModule({
   declarations: [AppComponent, CounterComponent],
@@ -11,6 +13,11 @@ import { counterReducer } from './counter.reducer';
     BrowserModule,
     AppRoutingModule,
     StoreModule.forRoot({ count: counterReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
