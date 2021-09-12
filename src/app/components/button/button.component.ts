48f56bc2b528fd989247d3f25d47ef8d78c2e34a
Mr.Richard <bnmghjtyu0@gmail.com>
2021-09-12 22:28:12 +0800

    button 組件加入 type、theme

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,21 +1,25 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+export type Type = 'outlined' | 'fill' | null;
+export type Theme = 'primary' | 'default' | null;
+
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
+  host: {
+    '[class.btn-primary]': `theme === 'primary'`,
+    '[class.btn-outlined]': `type === 'outlined'`,
+  },
   styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent implements OnInit {
-  @Input()
-  get vertical(): boolean {
-    return this._vertical;
-  }
-  set vertical(value: boolean) {
-    this._vertical = true;
-  }
-  private _vertical: boolean = false;
+  @Input() type: Type = 'outlined';
+  @Input() theme: Theme = 'default';
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(this.type);
+    console.log(this.theme);
+  }
 }
