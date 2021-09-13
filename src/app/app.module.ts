e3d38331febc6581c898a70d556c6c23ba832327
Mr.Richard <bnmghjtyu0@gmail.com>
2021-09-13 22:00:16 +0800

    chore: 完成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesModule } from './pages/page.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DemoMaterialModule } from './material-modules';
 import { A11yNavbarComponent } from './components/a11y-navbar/a11y-navbar.component';
+import { FormComponent } from './components/form/form.component';
 @NgModule({
-  declarations: [AppComponent, A11yNavbarComponent],
+  declarations: [AppComponent, A11yNavbarComponent, FormComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     PagesModule,
     DemoMaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/form/validator.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { FormGroup, ValidatorFn } from '@angular/forms';
+import { Room } from './form';
+@Injectable({ providedIn: 'root' })
+export class RoomOver18Validator {
+  public onlyAccessRoomsOver18(minAge: number): ValidatorFn {
+    return (formGroup: any) => {
+      const ageControl = formGroup.controls.age;
+      const roomControl = formGroup.controls.room;
+
+      if (!ageControl || !roomControl) {
+        return null;
+      }
+
+      const ageValue = ageControl.value;
+
+      if (!ageValue) {
+        return null;
+      }
+
+      if (ageValue >= 18) {
+        return null;
+      }
+
+      if (ageValue >= minAge) {
+        return null;
+      }
+
+      const roomsValue = roomControl.value as Room;
+
+      if (!roomsValue) {
+        return null;
+      }
+
+      if (roomsValue.value === 'room-2' || roomsValue.value === 'room-3') {
+        return { roomOnlyWith18: true }; // This is our error!
+      }
+
+      return null;
+    };
+  }
+}
