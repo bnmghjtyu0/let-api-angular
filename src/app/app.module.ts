e6b5fe379a4bae648124930ac22054cb6b468f65
Mr.Richard <bnmghjtyu0@gmail.com>
2021-09-13 22:00:16 +0800

    chore: 完成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DemoMaterialModule } from './material-modules';
 import { A11yNavbarComponent } from './components/a11y-navbar/a11y-navbar.component';
+import { FormComponent } from './components/form/form.component';
 @NgModule({
-  declarations: [AppComponent, A11yNavbarComponent],
+  declarations: [AppComponent, A11yNavbarComponent, FormComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     DemoMaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/form/form.component.ts
@@ -0,0 +1,55 @@
+import { RoomOver18Validator } from './validator.service';
+import { Component, OnInit } from '@angular/core';
+import {
+  Validators,
+  FormBuilder,
+  FormGroup,
+  AbstractControl,
+} from '@angular/forms';
+import { Room } from './form';
+
+function NoNegativeNumbers(control: AbstractControl) {
+  return control.value < 0 ? { negativeNumber: true } : null;
+}
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss'],
+})
+export class FormComponent implements OnInit {
+  title = 'forms-cross-field-validation';
+  profileForm!: FormGroup;
+
+  register() {
+    this.profileForm = this.formBuilder.group(
+      {
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        age: ['', [Validators.required, NoNegativeNumbers]],
+        room: [{}, Validators.required],
+      },
+      {
+        validators: [this.roomOver18Validator.onlyAccessRoomsOver18(18)],
+        updateOn: 'blur',
+      }
+    );
+  }
+
+  rooms: Room[] = [
+    { text: 'room 1', value: 'room-1' },
+    { text: 'room 2', value: 'room-2' },
+    { text: 'room 3', value: 'room-3' },
+  ];
+  constructor(
+    private formBuilder: FormBuilder,
+    private roomOver18Validator: RoomOver18Validator
+  ) {}
+
+  ngOnInit(): void {
+    this.register();
+  }
+  onSubmit() {
+    console.log(this.profileForm.value);
+  }
+}
