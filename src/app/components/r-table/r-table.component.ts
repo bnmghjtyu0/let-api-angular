beada41e4406488d3628dce3a897deac9dcf8f13
Mr.Richard <bnmghjtyu0@gmail.com>
2021-10-02 14:42:24 +0800

    add some types

--- src/app/components/r-table/r-table.component.ts
+++ src/app/components/r-table/r-table.component.ts
@@ -10,7 +10,7 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { mockDatas } from './mock/datas';
-import { Column } from './model/r-table';
+import { Column, Data } from './model/r-table';
 
 interface ViewContext<T> {
   $implicit: T;
@@ -111,9 +111,10 @@ export class RTableComponent {
   getRowSpan(col: string, index: number) {
     return this.spans[index] && this.spans[index][col];
   }
+
   getUserViewContext(
-    column: any,
-    data: any,
+    column: Column,
+    data: Data,
     dataIdx: number
   ): ViewContext<any> {
     return { $implicit: { column, data, i: dataIdx } };
