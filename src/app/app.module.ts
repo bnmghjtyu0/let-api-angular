af7525523adf16545cec605f20a769edf92d5c77
Mr.Richard <bnmghjtyu0@gmail.com>
2021-10-30 09:54:20 +0800

    2. 建立元件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ArticlesComponent } from './components/articles/articles.component';
+import { TagsComponent } from './components/tags/tags.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ArticlesComponent,
+    TagsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
