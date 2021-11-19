3fe108e56dba80ebffb68b917ca50d8d62d754a7
Mr.Richard <bnmghjtyu0@gmail.com>
2021-11-19 15:29:49 +0800

    chore: compodoc-todomvc finish

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -3,20 +3,20 @@ import { Component, OnInit, Input } from '@angular/core';
 import { TodoStoreService } from '../../shared/services/todo-store.service';
 
 /**
- * The todo component
+ * 這是一個 todo component
  * ```html
  * <todo>
  *    [todo]="todo"
  * </todo>
  * ```
  * <example-url>../screenshots/todo/todo.png</example-url>
+ * <example-url>http://localhost:4200</example-url>
  */
 @Component({
   selector: 'todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.scss'],
 })
-
 export class TodoComponent {
   /**
    * The entry todo from the parent list
@@ -39,12 +39,33 @@ export class TodoComponent {
   toggleCompletion(todo: Todo) {
     this.todoStore.toggleCompletion(todo);
   }
-
-  editTodo(todo: Todo) {
+  /**
+   * 編輯 todo 的資料
+   * ```typescript
+   *  editTodo({
+   *    completed: false,
+   *    editing: false,
+   *    title: "早安有喜",
+   *  })
+   * ```
+   * @param {todo} todo
+   * @return void
+   */
+  editTodo(todo: Todo): void {
     todo.editing = true;
   }
-
+  /**
+   * 停止編輯，更新 todo.title 的資料
+   *
+   * ```typescript
+   *  editTodo({ completed: false, editing: false, title: "早安有喜"},'編輯中')
+   * ```
+   * @param {Todo} todo
+   * @param {string} editedTitle input value
+   * @returns void
+   */
   stopEditing(todo: Todo, editedTitle: string) {
+    console.log(editedTitle);
     todo.title = editedTitle;
     todo.editing = false;
   }
--- src/app/shared/models/todo.model.ts
+++ src/app/shared/models/todo.model.ts
@@ -6,6 +6,7 @@
 export class Todo {
   /**
    * Completed status
+   * @type {boolean}
    */
   completed: boolean;
   /**
