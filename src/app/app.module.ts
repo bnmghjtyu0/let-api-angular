5117beb2cf2a18d1d397ee4135b565152035e1f8
Mr.Richard <bnmghjtyu0@gmail.com>
2022-07-16 13:39:15 +0800

    chore: 加入 material-ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesModule } from './pages/page.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedMaterialModule } from './material/shared-material.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, PagesModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PagesModule,
+    BrowserAnimationsModule,
+    SharedMaterialModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
