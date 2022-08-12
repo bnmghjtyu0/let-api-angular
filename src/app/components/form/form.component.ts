b7ccfd6330a22201b25670ace7f806817c203435
Mr.Richard <bnmghjtyu0@gmail.com>
2022-08-12 14:36:00 +0800

    chore: FormArray 中切換 select 變更 input 欄位

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -6,6 +6,7 @@ import {
   FormGroup,
   AbstractControl,
   FormControl,
+  FormArray,
 } from '@angular/forms';
 import { Room } from './form';
 
@@ -19,41 +20,89 @@ function NoNegativeNumbers(control: AbstractControl) {
   styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
+  Object = Object;
   title = 'forms-cross-field-validation';
   profileForm!: FormGroup;
-
-  group1 = {
-    firstName: ['', Validators.required],
-    lastName: ['', Validators.required],
-    age: ['', [Validators.required, NoNegativeNumbers]],
-    room: [null, Validators.required],
-  };
-
-  register() {
-    this.profileForm = this.formBuilder.group(this.group1, {
-      validators: [this.roomOver18Validator.onlyAccessRoomsOver18(18)],
-      updateOn: 'blur',
-    });
-  }
 
   rooms: Room[] = [
     { text: 'room 1', value: 'room-1' },
     { text: 'room 2', value: 'room-2' },
     { text: 'room 3', value: 'room-3' },
   ];
+
+  get profileArrayControls() {
+    return (this.profileForm.controls.profileArray as FormArray).controls;
+  }
+
   constructor(
-    private formBuilder: FormBuilder,
+    private fb: FormBuilder,
     private roomOver18Validator: RoomOver18Validator
   ) {}
 
   ngOnInit(): void {
-    this.register();
+    this.profileForm = this.fb.group(
+      {
+        firstName: this.fb.control('', { validators: [Validators.required] }),
+        lastName: this.fb.control('', { validators: [Validators.required] }),
+        profileArray: this.fb.array([]),
+        age: this.fb.control('', {
+          validators: [Validators.required, NoNegativeNumbers],
+        }),
+        room: this.fb.control(null, {
+          validators: [Validators.required],
+          updateOn: 'change',
+        }),
+      },
+      {
+        validators: [this.roomOver18Validator.onlyAccessRoomsOver18(18)],
+        updateOn: 'submit',
+      }
+    );
   }
   /* Handle form errors in Angular 8 */
   public errorHandling = (control: string, error: string) => {
     return this.profileForm.controls[control].hasError(error);
   };
 
+  get profileArray() {
+    return (this.profileForm.controls.profileArray as FormArray).controls;
+  }
+
+  changeSelectArray($event: any, index: number) {
+    const currentArray = (
+      this.profileForm.controls.profileArray as FormArray
+    ).at(index) as FormGroup;
+
+    // room-1 單選(singleCity)
+    // room-2 多選(multipleCity)
+    // room-3 自定義(customCity)
+    if ($event.value.value === 'room-1') {
+      currentArray.removeControl('multipleCity');
+      currentArray.removeControl('customCity');
+      currentArray.addControl(
+        `singleCity`,
+        this.fb.control('', {
+          validators: [Validators.required],
+        })
+      );
+    }
+    if ($event.value.value === 'room-2') {
+      currentArray.removeControl('singleCity');
+      currentArray.removeControl('customCity');
+      currentArray.addControl(
+        `multipleCity`,
+        new FormControl('', Validators.required)
+      );
+    }
+    if ($event.value.value === 'room-3') {
+      currentArray.removeControl('singleCity');
+      currentArray.removeControl('multipleCity');
+      currentArray.addControl(
+        `customCity`,
+        new FormControl('', Validators.required)
+      );
+    }
+  }
   changeSelect($event: any) {
     if ($event.value.value === 'room-1') {
       this.profileForm.removeControl('firstName2');
@@ -71,8 +120,27 @@ export class FormComponent implements OnInit {
     }
   }
 
+  /**
+   * 新增一個 array
+   */
+  createProfileArray(): FormGroup {
+    return this.fb.group({
+      singleCity: this.fb.control('', {
+        validators: [Validators.required],
+      }),
+      omgRoom: this.fb.control('', {
+        validators: [Validators.required],
+      }),
+    });
+  }
+
+  addProfileArray() {
+    (this.profileForm.controls.profileArray as FormArray).push(
+      this.createProfileArray()
+    );
+  }
+
   onSubmit() {
-    // this.profileForm.controls.room.setValue(null);
     console.log(this.profileForm.value);
   }
 }
